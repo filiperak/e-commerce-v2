b0a3448966990acb03bbddd582124932593002e9
filiperak <filip.erak@googlemail.com>
2024-04-25 11:51:31 +0200

    'header'

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { HeaderContainer } from './styled'
+import React from "react";
+import { ReactComponent as Arrow } from "../../assets/arrow.svg";
+import { HeaderContainer, HeaderRigth, SearchBar } from "./styled";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SearchIcon from "@mui/icons-material/Search";
+
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 
 const Header = () => {
   return (
     <HeaderContainer>
-        
+      <Arrow/>
+      <SearchBar>
+        <input type="text" placeholder="Search products..." />
+        <SearchIcon />
+      </SearchBar>
+      <HeaderRigth>
+        <PersonOutlineIcon />
+        <ShoppingCartIcon />
+        <span>0</span>
+      </HeaderRigth>
     </HeaderContainer>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/header/styled.js
+++ src/components/header/styled.js
@@ -1,5 +1,39 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-    
+    background-color: var(--main-gray);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .MuiSvgIcon-root{
+        cursor: pointer;
+    }
+    .MuiSvgIcon-root:hover{
+        color: var(--main-orange);
+    }
+
+`;
+export const SearchBar = styled.div`
+    display: flex;
+    border-radius: 4px;
+    background-color: #fff;
+    padding: 2px;
+    flex: 0.7;
+    >input{
+        border: none;
+        flex:0.9%;
+    }
+    >input:focus{
+        outline: none;
+    }
+`;
+export const HeaderRigth = styled.div`
+    flex:0.1;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-right: 2%;
+    > .MuiSvgIcon-root:first-child{
+        margin-right: auto;
+    }
 `;
