9ac1d23f4c85a9257486e8ab8405c4ef6d51a889
filiperak <filip.erak@googlemail.com>
2024-05-11 19:40:09 +0200

    admin delete method

--- src/components/views/admin/Admin.jsx
+++ src/components/views/admin/Admin.jsx
@@ -1,16 +1,76 @@
-import React from 'react'
-import { AdminContainer, OpacityContainer } from './styled'
-import ProductList from '../../products/ProductList'
+import React, { useEffect, useState } from "react";
+import {
+  AdminContainer,
+  AdminProductList,
+  AdminProductListItem,
+  AdminProductListItemInfo,
+  ProductIcons,
+  ProductInfo,
+} from "./styled";
+import { api } from "../../../services/api";
+import { Link } from "react-router-dom";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 const Admin = () => {
-  return <>
-    <AdminContainer>
-        <h3>This is a admin page. Here you can update, create and delete products.</h3>
-        <OpacityContainer>
-            <ProductList/>
-        </OpacityContainer>
-    </AdminContainer>
-  </>
-}
+  const [products, setProducts] = useState([]);
 
-export default Admin
+  async function getProducts(URL) {
+    try {
+      let apiUrl = (URL += `?limit=100`);
+      const response = await fetch(apiUrl);
+      const result = await response.json();
+      setProducts(result.products);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  useEffect(() => {
+    getProducts(api);
+  }, []);
+
+  async function deleteElement(elemId) {
+    try {
+      const response = await fetch(`${api}/${elemId}`, {
+        method: "DELETE",
+      });
+      const result = await response.json();
+      if (response.ok) {
+        setProducts(products.filter((product) => product.id !== elemId));
+      } else {
+        console.error("Failed to delete product:", result.error); // Log error message if deletion fails
+      }
+    } catch (error) {
+      console.error("Error deleting product:", error);
+    }
+  }
+  
+  return (
+    <>
+      <AdminContainer>
+        <h3>
+          This is a admin page. Here you can update, create and delete products.
+        </h3>
+        <AdminProductList>
+          {products.map((elem) => {
+            return (
+              <AdminProductListItem>
+                <ProductInfo>
+                  <p>{`ID:${elem.id}`}</p>
+                  <img src={elem.thumbnail} />
+                  <p>{elem.title}</p>
+                </ProductInfo>
+                <ProductIcons>
+                  <EditIcon />
+                  <DeleteOutlineIcon onClick={() => deleteElement(elem.id)} />
+                </ProductIcons>
+              </AdminProductListItem>
+            );
+          })}
+        </AdminProductList>
+      </AdminContainer>
+    </>
+  );
+};
+
+export default Admin;
--- src/components/views/admin/styled.js
+++ src/components/views/admin/styled.js
@@ -2,7 +2,30 @@ import styled from "styled-components";
 
 export const AdminContainer = styled.div`
     margin-top: var(--main-component-margin);
+    max-width: 1100px;
+    margin-left: auto;
+    margin-right: auto;
 `;
-export const OpacityContainer = styled.div`
-    opacity: 80%;
+export const AdminProductList = styled.ul`
+    display: flex;
+    flex-direction: column;
+`;
+export const AdminProductListItem = styled.li`
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid var(--main-gray);
+    padding: 10px;
+
+`;
+export const ProductInfo = styled.div`
+    display: flex;
+    align-items: center;
+    >img{
+        height: 50px;
+        width: 50px;
+        object-fit: contain;
+    }
+`;
+export const ProductIcons = styled.div`
+
 `;
